358bbe1aebfc1d81174411f08d4d4f0bd4373f15
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-06-29 12:50:23 +0300

    little bit more

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,7 @@
 const categories = document.querySelectorAll('.item');
 console.log('Number of categories:', categories.length);
 
+const itemCategory = categories.forEach(function(category) {s
+    return console.log(category);
+})
+
