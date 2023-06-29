b86a94f52847f373e0ea611051a0749766a3ff06
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-06-29 15:03:44 +0300

    Second done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.getElementById('ingredients');
+// console.log(list);
+
+const itemIngridientsArr = ingredients.map((itemData) => {
+  const itemElement = document.createElement('li');
+  itemElement.textContent = itemData;
+  itemElement.classList.add('item');
+  return itemElement;
+})
+
+// console.log(itemIngridientsArr);
+
+list.append(...itemIngridientsArr);
+
