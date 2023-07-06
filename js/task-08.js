f39e12e2342cd60e4fd36f3f5d2586cbbf45d2cf
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-07-06 11:39:13 +0300

    8th done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const formElement = document.querySelector(".login-form");
+formElement.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return console.log("Please fill in all fields!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+
+  event.currentTarget.reset();
+}
