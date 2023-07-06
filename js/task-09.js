9d7a5cc92100ea1cc1e176e331d8826e5a2fe6f5
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-07-06 11:56:28 +0300

    9th done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const changeColorBtn = document.querySelector('.change-color');
+const colorName = document.querySelector('.color');
+
+changeColorBtn.addEventListener ('click' , colorChange);
+
+function colorChange (event) {
+  const newColor = getRandomHexColor ();
+  body.style.backgroundColor = newColor;
+  colorName.textContent = newColor;
+}
