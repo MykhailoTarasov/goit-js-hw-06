dfa7a9f4c3005a971a774ab07ad77fd4ed301d85
MykhailoTarasov <123101959+MykhailoTarasov@users.noreply.github.com>
2023-07-06 11:19:55 +0300

    7th done

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+const inputElement = document.getElementById('font-size-control');
+const textElement = document.getElementById('text');
+
+inputElement.addEventListener ('input', change);
+
+function change () {
+    textElement.style.fontSize = inputElement.value + "px";
+};
